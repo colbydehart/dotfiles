39c1a29f0373d917fb0145851a882983481738db
Colby DeHart <colbydehart@gmail.com>
2017-06-27 15:39:31 -0400

    added atom files

--- init.js
+++ init.js
@@ -1,6 +1,21 @@
+exec = require('child_process').exec
+
 atom.commands.add('atom-text-editor', 'cool:close-panes', () => {
-  const panes = atom.workspace.getPanes()
-  const activePane = atom.workspace.getActivePane()
+  const panes = atom.workspace.getPanes();
+  const activePane = atom.workspace.getActivePane();
   for (let pane of panes)
-    pane !== activePane && pane.destroy()
-})
+    pane !== activePane && pane.destroy();
+});
+
+atom.commands.add('atom-text-editor', 'cool:print-grammar', () => {
+  const gram = atom.workspace.getActiveTextEditor().getGrammar().scopeName;
+  atom.notifications.addInfo(gram);
+});
+
+atom.commands.add('atom-text-editor', 'cool:scratch', () => {
+  atom.workspace.open("/Users/colbydehart/.scratch")
+});
+
+atom.commands.add('atom-text-editor', 'cool:org', () => {
+  atom.workspace.open("/Users/colbydehart/org/gtd.org")
+});
